5b985f2163219f2a7d56539b420fc9a48e6e8367
shivashanmugam <sivashanmugam4444@gmail.com>
2020-03-03 10:19:29 +0530

    special string over

--- problem_solving/hackerrank/interview-preparation-kit/string_manipulation/special_string/test/special_string.spec.js
+++ problem_solving/hackerrank/interview-preparation-kit/string_manipulation/special_string/test/special_string.spec.js
@@ -1,11 +1,23 @@
 var getTotalSpecialStrings = require('../special_string').getTotalSpecialStrings;
 var chai = require('../../../../../../libs/chai/chai');
 var expect = chai.expect;
+var fs = require('fs')
+var data = fs.readFileSync('text.txt')
+
 
 describe('getTotalSpecialStrings', function () {
     it('Testing getTotalSpecialStrings with different test cases', function () {
-        expect(getTotalSpecialStrings('aaaaa')).to.equal('YES');
-        dferwrnnnonkmkkwewknbqweaabbcbcccdcccaccccccabcczhijkcccddddabcdasaasdasasdabcbaba
-        var str = 'aabbcbcccdcccaccccccabcczhijkcccddddabcdasaasdasasdabcbaba'
+        expect(getTotalSpecialStrings('aaaaa')).to.equal(15);
+        expect(getTotalSpecialStrings('abcbaba')).to.equal(10);
+        expect(getTotalSpecialStrings('mnonopoo')).to.equal(12);
+        expect(getTotalSpecialStrings('asasd')).to.equal(7);
+        expect(getTotalSpecialStrings('mmmoaaa')).to.equal(13);
+        expect(getTotalSpecialStrings('mmmomaa')).to.equal(12);
+        expect(getTotalSpecialStrings('mmmomma')).to.equal(13);
+        expect(getTotalSpecialStrings('mmaoaaa')).to.equal(12);
+        expect(getTotalSpecialStrings('maaoaaa')).to.equal(13);
+        expect(getTotalSpecialStrings('mamaabaaacaaaba')).to.equal(30);
+        expect(getTotalSpecialStrings(data)).to.equal(1272919)
     })
+    
 })
